640cabbcc63db390201b469b599b81f457ec01e2
Madhur Manekar <63803380+LORD-MM@users.noreply.github.com>
2023-04-28 03:04:22 +0530

    Update index.js

--- Mini Project/index.js
+++ Mini Project/index.js
@@ -1,111 +1,163 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
+// // const express = require("express");
+// // const mongoose = require("mongoose");
+// // const app = express();
+
+// // const uri = "mongodb+srv://madhurmanekar:atlasuser01@nutri-poshan.t2pg1xv.mongodb.net/test";
+
+// // async function connect() {
+// //     try {
+// //         await mongoose.connect(uri);
+// //         console.log("connected to Mongodb")
+// //     } catch (error) {
+// //         console.error(error);
+// //     }
+// // }
+
+// // connect();
+
+// // app.listen(8000, () => {
+// //     console.log("Server started on post 8000");
+// // })
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+
+// // Create an instance of the express application
 // const app = express();
 
-// const uri = "mongodb+srv://madhurmanekar:atlasuser01@nutri-poshan.t2pg1xv.mongodb.net/test";
+// // Connect to the MongoDB Atlas cluster
+// mongoose.connect('mongodb+srv://madhurmanekar:atlasuser01@nutri-poshan.t2pg1xv.mongodb.net', { useNewUrlParser: true, useUnifiedTopology: true })
+// //   .then(() => {
+// //     console.log('Connected to MongoDB Atlas');
+// //   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
-// async function connect() {
-//     try {
-//         await mongoose.connect(uri);
-//         console.log("connected to Mongodb")
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
+// // Define the MongoDB schema and model for the institute collection
+// const InstituteSchema = new mongoose.Schema({
+//   instituteName: {
+//     type: String,
+//     required: true
+//   },
+//   instituteEmail: {
+//     type: String,
+//     required: true
+//   },
+//   instituteAddress: {
+//     type: String,
+//     required: true
+//   },
+//   moderatorName: {
+//     type: String,
+//     required: true
+//   },
+//   moderatorEmail: {
+//     type: String,
+//     required: true
+//   },
+//   moderatorPhone: {
+//     type: String,
+//     required: true
+//   },
+//   /*institutePass: {
+//     type: String,
+//     required: true
+//   }*/
+// });
 
-// connect();
+// //console.log(InstituteSchema.instituteName);
+// const Institute = mongoose.model('Institute', InstituteSchema);
 
-// app.listen(8000, () => {
-//     console.log("Server started on post 8000");
-// })
+// // Parse incoming form data
+// app.use(express.urlencoded({ extended: true }));
+
+// // Handle form submission
+// app.get('/signup', async (req, res) => {
+  
+//   console.log(req.body);
+  
+//   const {
+//     instituteName,
+//     instituteEmail,
+//     instituteAddress,
+//     moderatorName,
+//     moderatorEmail,
+//     moderatorPhone,
+//     //institutePass
+//   } = req.body;
+
+//   // Create a new institute document
+//   const institute = new Institute({
+//     instituteName,
+//     instituteEmail,
+//     instituteAddress,
+//     moderatorName,
+//     moderatorEmail,
+//     moderatorPhone,
+//     //institutePass
+//   });
+
+//   try {
+//     // Save the document to the database
+//     await institute.save();
+//     res.send('Form submitted successfully');
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Internal server error');
+//   }
+// });
+
+// // Start the server
+// app.listen(3000, () => {
+//   console.log('Server started on port 3000');
+// });
+
 
 const express = require('express');
 const mongoose = require('mongoose');
 
-// Create an instance of the express application
-const app = express();
+// Connect to MongoDB server
+mongoose.connect('mongodb+srv://madhurmanekar:atlasuser01@nutri-poshan.t2pg1xv.mongodb.net', { useNewUrlParser: true });
 
-// Connect to the MongoDB Atlas cluster
-mongoose.connect('mongodb+srv://madhurmanekar:atlasuser01@nutri-poshan.t2pg1xv.mongodb.net', { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connected to MongoDB Atlas');
-//   })
-  .catch((error) => {
-    console.error(error);
-  });
-
-// Define the MongoDB schema and model for the institute collection
-const InstituteSchema = new mongoose.Schema({
-  instituteName: {
-    type: String,
-    required: true
-  },
-  instituteEmail: {
-    type: String,
-    required: true
-  },
-  instituteAddress: {
-    type: String,
-    required: true
-  },
-  moderatorName: {
-    type: String,
-    required: true
-  },
-  moderatorEmail: {
-    type: String,
-    required: true
-  },
-  moderatorPhone: {
-    type: String,
-    required: true
-  },
-  /*institutePass: {
-    type: String,
-    required: true
-  }*/
+// Define a schema for the data
+const instituteSchema = new mongoose.Schema({
+  instituteName: String,
+  instituteEmail: String,
+  instituteAddress: String,
+  moderatorName: String,
+  moderatorEmail: String,
+  moderatorPhone: String
 });
 
-//console.log(InstituteSchema.instituteName);
-const Institute = mongoose.model('Institute', InstituteSchema);
+// Create a model based on the schema
+const Institute = mongoose.model('Institute', instituteSchema);
+
+// Create an express app
+const app = express();
 
 // Parse incoming form data
 app.use(express.urlencoded({ extended: true }));
 
-// Handle form submission
-app.get('/signup', async (req, res) => {
-  
-  console.log(req.body);
-  
-  const {
-    instituteName,
-    instituteEmail,
-    instituteAddress,
-    moderatorName,
-    moderatorEmail,
-    moderatorPhone,
-    //institutePass
-  } = req.body;
-
-  // Create a new institute document
-  const institute = new Institute({
-    instituteName,
-    instituteEmail,
-    instituteAddress,
-    moderatorName,
-    moderatorEmail,
-    moderatorPhone,
-    //institutePass
+// Handle form submissions
+app.post('/signup', (req, res) => {
+  const newInstitute = new Institute({
+    instituteName: req.body.instituteName,
+    instituteEmail: req.body.instituteEmail,
+    instituteAddress: req.body.instituteAddress,
+    moderatorName: req.body.moderatorName,
+    moderatorEmail: req.body.moderatorEmail,
+    moderatorPhone: req.body.moderatorPhone
   });
 
-  try {
-    // Save the document to the database
-    await institute.save();
-    res.send('Form submitted successfully');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error');
-  }
+  newInstitute.save((err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error saving data to database');
+    } else {
+      res.send('Data saved to database');
+    }
+  });
 });
 
 // Start the server
