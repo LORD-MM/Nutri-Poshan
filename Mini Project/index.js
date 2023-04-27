2e26a26722193eda04d94c18bc748b60101ca284
Madhur Manekar <63803380+LORD-MM@users.noreply.github.com>
2023-04-28 01:31:53 +0530

    Update index.js

--- Mini Project/index.js
+++ Mini Project/index.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const app = express();
 
-const uri = "mongodb+srv://madhurmanekar:atlasuser01@nutri-poshan.t2pg1xv.mongodb.net/test";
+// const uri = "mongodb+srv://madhurmanekar:atlasuser01@nutri-poshan.t2pg1xv.mongodb.net/test";
 
-async function connect() {
-    try {
-        await mongoose.connect(uri);
-        console.log("connected to Mongodb")
-    } catch (error) {
-        console.error(error);
-    }
-}
+// async function connect() {
+//     try {
+//         await mongoose.connect(uri);
+//         console.log("connected to Mongodb")
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
 
-connect();
+// connect();
 
-app.listen(8000, () => {
-    console.log("Server started on post 8000");
-})
+// app.listen(8000, () => {
+//     console.log("Server started on post 8000");
+// })
 
 const express = require('express');
 const mongoose = require('mongoose');
